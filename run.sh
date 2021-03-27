e01b5fcff7ae895cc542dfca6ea0d3b300c13ea4
Tiger Nie <nhl0819@gmail.com>
2021-03-27 18:39:35 +0800

    script to run all benchmarks

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+### python
+cd py3
+
+# python3
+py3=$(python3 main.py)
+echo python3: $py3 passes
+
+# pypy3
+pypy3=$(pypy3 main.py)
+echo pypy3: $pypy3 passes
+
+# cython3
+./build_cython.sh > /dev/null
+cython3=$(python3 main_cython.py)
+echo cython3: $cython3 passes
+
+cd ..
+
+
