4a42887f7951d9d42e70cc654085525de68ff9b2
Tiger Nie <nhl0819@gmail.com>
2021-03-27 19:32:22 +0800

    optimized cython version

--- py3/setup.py
+++ py3/setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules = cythonize("sievec.pyx")
+    ext_modules = cythonize("sievec.pyx", annotate=True, compiler_directives={'language_level' : "3"})
 )
 
