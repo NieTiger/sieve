4a3ac56b89912fe72b7d8a2d46975525134eee9c
Tiger Nie <tnie@tuta.io>
2021-11-19 20:47:59 -0600

    disable numba for now as i can't test it on my machine

--- py3/run.sh
+++ py3/run.sh
@@ -5,12 +5,12 @@ echo CPython3
 python3 --version
 echo
 
-# CPython3 with numba
-python3 main_numba.py
-echo CPython3 + numba
-python3 --version
-python3 -c "import numpy; print('Numba', numpy.__version__)"
-echo
+## CPython3 with numba
+#python3 main_numba.py
+#echo CPython3 + numba
+#python3 --version
+#python3 -c "import numpy; print('Numba', numpy.__version__)"
+#echo
 
 pypy3 main.py
 echo pypy3
