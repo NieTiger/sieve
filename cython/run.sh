90e760fe921c9f2c0f8b0974b0a31300abf8d9e0
Tiger Nie <tnie@tuta.io>
2022-02-24 11:54:40 -0600

    improved cython

--- cython/run.sh
+++ cython/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 make 2>&1 1>/dev/null
-echo Cython
 python3 main.py
+echo Cython
 python3 -c "import Cython; print('Cython', Cython.__version__)"
 python3 --version
 echo
--- cython/sieve.pyx
+++ cython/sieve.pyx
@@ -1,11 +1,15 @@
 # distutils: language=c++
 from libcpp.vector cimport vector
 from libc.math cimport sqrt
+cimport cython
 
+
+@cython.boundscheck(False)
 def sieve(unsigned int size):
     cdef unsigned int factor, q, num
     cdef vector[unsigned int] a
     cdef vector[unsigned int] res
+    cdef unsigned int step
 
     if size < 2:
         return a
@@ -20,8 +24,15 @@ def sieve(unsigned int size):
                 factor = num
                 break
 
-        for num in range(factor * factor, size, factor * 2):
+        step = factor * 2
+        num = factor * factor
+        while num < size:
             a[num] = 1
+            num += step
+
+        # Cython cannot release the GIL when the range step is unknown at compile time
+        # for num in range(factor * factor, size, factor * 2):
+            # a[num] = 1
 
         factor += 2
 
