df25f72aa9a0391e997bdb97acabc45475b387ca
Tiger Nie <tnie@tuta.io>
2022-02-24 11:56:28 -0600

    golang use int array instead of bool

--- go/sieve.go
+++ go/sieve.go
@@ -9,20 +9,20 @@ func Sieve(size int) []int {
 		return []int{}
 	}
 
-	a := make([]bool, size)
+	a := make([]uint, size)
 	q := int(math.Sqrt(float64(size)))
 	factor := 3
 
 	for factor < q {
 		for i := factor; i < size; i += 2 {
-			if !a[i] {
+			if a[i] == 0 {
 				factor = i
 				break
 			}
 		}
 
 		for i := factor * factor; i < size; i += factor * 2 {
-			a[i] = true
+			a[i] = 1
 		}
 
 		factor += 2
@@ -30,7 +30,7 @@ func Sieve(size int) []int {
 
 	res := []int{2}
 	for i := 3; i < size; i += 2 {
-		if !a[i] {
+		if a[i] == 0 {
 			res = append(res, i)
 		}
 	}
