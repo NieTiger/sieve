eae1cf1336731a6159da4ebe0e5334a6663002c8
Tiger Nie <tnie@tuta.io>
2022-08-06 00:14:21 -0500

    optimized cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -4,34 +4,38 @@
 #include <iostream>
 #include <vector>
 
+
 std::vector<unsigned int> sieve(unsigned int size) {
-  if (size < 2)
-    return {};
+    if (size < 2)
+        return {};
 
-  auto a = std::vector<unsigned int>(size);
-  unsigned int q = std::sqrt(size);
-  unsigned int factor = 3;
+    // because all prime numbers (except 2) must be odd, we can bit-shift all numbers to the right by 1
+    // and reduce our memory usage by half
+    auto a = std::vector<unsigned int>(size >> 1);
+    const unsigned int q = std::sqrt(size);
+    unsigned int factor = 3;
 
-  while (factor < q) {
-    for (unsigned int i = factor; i < size; i += 2) {
-      if (!a[i]) {
-        factor = i;
-        break;
-      }
+    while (factor < q) {
+        for (unsigned int i = factor; i < size; i += 2)
+            if (!a[i>>1]) {
+                factor = i;
+                break;
+            }
+
+        for (unsigned int i = factor * factor; i < size; i += factor * 2)
+            a[i>>1] = 1;
+
+        factor += 2;
     }
 
-    for (unsigned int i = factor * factor; i < size; i += factor * 2)
-      a[i] = 1;
-
-    factor += 2;
-  }
-
-  std::vector<unsigned int> res = {2};
-  for (unsigned int i = 3; i < size; i += 2) {
-    if (!a[i])
-      res.push_back(i);
-  }
-  return res;
+    a[0] = 2;
+    unsigned int n_primes_found = 1;
+    for (unsigned int i = 3; i < size; i += 2) {
+        if (!a[i>>1])
+            a[n_primes_found++] = i;
+    }
+    a.erase(a.begin() + n_primes_found, a.end());
+    return a;
 }
 
 std::vector<unsigned int> load_truth() {
