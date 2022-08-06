4fcb9663e61fc35b22956cd8d9ab5b4563ebcd18
Tiger Nie <tnie@tuta.io>
2022-08-06 01:06:10 -0500

    optimized go

--- go/sieve.go
+++ go/sieve.go
@@ -9,31 +9,34 @@ func Sieve(size int) []int {
 		return []int{}
 	}
 
-	a := make([]uint, size)
+	a := make([]int, size/2)
 	q := int(math.Sqrt(float64(size)))
 	factor := 3
 
 	for factor < q {
 		for i := factor; i < size; i += 2 {
-			if a[i] == 0 {
+			if a[i>>1] == 0 {
 				factor = i
 				break
 			}
 		}
 
 		for i := factor * factor; i < size; i += factor * 2 {
-			a[i] = 1
+			a[i>>1] = 1
 		}
 
 		factor += 2
 	}
 
-	res := []int{2}
+	a[0] = 2
+	prime_idx := 1
 	for i := 3; i < size; i += 2 {
-		if a[i] == 0 {
-			res = append(res, i)
+		if a[i>>1] == 0 {
+			//res = append(res, i)
+			a[prime_idx] = i
+			prime_idx += 1
 		}
 	}
 
-	return res
+  return a[0:prime_idx]
 }
