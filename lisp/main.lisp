256202ecc0e851a8bed17a582c21f58e8e47a638
TN <tnie@tuta.io>
2022-10-19 19:52:09 -0500

    big improvements to common lisp impl

--- lisp/main.lisp
+++ lisp/main.lisp
@@ -1,15 +1,36 @@
 (declaim (optimize (speed 2) (safety 0)))
+
 (load "sieve.lisp")
 
-(defun bench ()
-  (do ((end (+ (get-internal-real-time) (* 5 internal-time-units-per-second)))
-       (passes 0 (1+ passes)))
-      ((> (get-internal-real-time) end)
-       passes)
-      (sieve 1000000)))
+(defun load-file (fname)
+  (with-open-file (stream fname)
+    (loop for x = (read stream nil)
+          while x
+            collect x)))
+
+(defun compare-lists (l1 l2)
+  (assert (eql (length l1) (length l2))
+          (l1 l2)
+          "List have different lengths.")
+  (loop for x1 in l1
+        for x2 in l2
+        do (assert (eql x1 x2)))
+  t)
+
+; load truth file and check results
+(defvar truth (load-file "../truth.txt"))
+(defvar res (sieve 1000000))
+(compare-lists truth res)
+
 
 (defun main ()
-  (write (bench))
-  (terpri))
+  (do ((end (+ (get-internal-real-time) (* 5 internal-time-units-per-second)))
+       (passes 0 (+ 1 passes)))
+      ((> (get-internal-real-time) end)
+       (format t "~a~%" passes))
+    (declare (type (unsigned-byte 64) passes)
+             (type (unsigned-byte 64) end))
+    (sieve 1000000)))
 
-(save-lisp-and-die "sieve" :executable t :toplevel #'main)
+; (main)
+ (save-lisp-and-die "sieve" :executable t :toplevel #'main)
